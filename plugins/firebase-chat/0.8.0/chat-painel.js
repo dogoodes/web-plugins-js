a3c2f7116cc29a80341cb3dac04deb2285b42704
albertocerqueira <alberto.cerqueira1990@gmail.com>
2015-10-19 18:24:16 -0200

    feat(code) : adjustments
    - adjustment firebase chat
    
    #Closed NoIssues

--- plugins/firebase-chat/0.8.0/chat-painel.js
+++ plugins/firebase-chat/0.8.0/chat-painel.js
@@ -1,4 +1,4 @@
-var admin = {id: "albertocerqueira", nome: "Alberto Cerqueira"};
+var admin = {id: "g6technology", nome: "G6 | Technology"};
 var lista = new Firebase('https://g6-chat.firebaseIO.com/chat/usuarios/');
 
 jQuery.chatPainel = (function(usuario) {
@@ -23,7 +23,7 @@ jQuery.chatPainel = (function(usuario) {
             chat.on('child_added', function(snap) {
                 var mensagem = snap.val();
                 
-                _self.mostrarMensagem(mensagem, DateUtils.time({timestamp: mensagem.timestamp}), {id: mensagem.id, nome: mensagem.usuario.nome});
+                _self.mostrarMensagem(mensagem, date.time({timestamp: mensagem.timestamp}), {id: mensagem.usuario.id, nome: mensagem.usuario.nome});
             });
             
             lista.on('child_added', function(snap) {
@@ -31,7 +31,7 @@ jQuery.chatPainel = (function(usuario) {
                 
                 var user = '<div class="media conversation" data-id="' + usuario.id + '">' +
                            '<a class="pull-left" href="?' + usuario.id + '=' + usuario.nome + '">' +
-                           '<img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 50px; height: 50px;" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAACqUlEQVR4Xu2Y60tiURTFl48STFJMwkQjUTDtixq+Av93P6iBJFTgg1JL8QWBGT4QfDX7gDIyNE3nEBO6D0Rh9+5z9rprr19dTa/XW2KHl4YFYAfwCHAG7HAGgkOQKcAUYAowBZgCO6wAY5AxyBhkDDIGdxgC/M8QY5AxyBhkDDIGGYM7rIAyBgeDAYrFIkajEYxGIwKBAA4PDzckpd+322243W54PJ5P5f6Omh9tqiTAfD5HNpuFVqvFyckJms0m9vf3EY/H1/u9vb0hn89jsVj8kwDfUfNviisJ8PLygru7O4TDYVgsFtDh9Xo9NBrNes9cLgeTybThgKenJ1SrVXGf1WoVDup2u4jFYhiPx1I1P7XVBxcoCVCr1UBfTqcTrVYLe3t7OD8/x/HxsdiOPqNGo9Eo0un02gHkBhJmuVzC7/fj5uYGXq8XZ2dnop5Mzf8iwMPDAxqNBmw2GxwOBx4fHzGdTpFMJkVzNB7UGAmSSqU2RoDmnETQ6XQiOyKRiHCOSk0ZEZQcUKlU8Pz8LA5vNptRr9eFCJQBFHq//szG5eWlGA1ywOnpqQhBapoWPfl+vw+fzweXyyU+U635VRGUBOh0OigUCggGg8IFK/teXV3h/v4ew+Hwj/OQU4gUq/w4ODgQrkkkEmKEVGp+tXm6XkkAOngmk4HBYBAjQA6gEKRmyOL05GnR99vbW9jtdjEGdP319bUIR8oA+pnG5OLiQoghU5OElFlKAtCGr6+vKJfLmEwm64aosd/XbDbbyIBSqSSeNKU+HXzlnFAohKOjI6maMs0rO0B20590n7IDflIzMmdhAfiNEL8R4jdC/EZIJj235R6mAFOAKcAUYApsS6LL9MEUYAowBZgCTAGZ9NyWe5gCTAGmAFOAKbAtiS7TB1Ng1ynwDkxRe58vH3FfAAAAAElFTkSuQmCC" />' +
+                           '<img class="media-object" alt="Avatar" style="width: 50px; height: 50px;" src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" />' +
                            '</a>' +
                            '<div class="media-body">' +
                            '<a class="pull-left ' + ((usuario.visualizado) ? 'visualizado' : '') + '" href="?' + usuario.id + '=' + usuario.nome + '">' +
@@ -68,15 +68,15 @@ jQuery.chatPainel = (function(usuario) {
         
         this.mostrarMensagem = (function (mensagem, time, usuario) {
             var msg = '<div class="media msg" data-uuid="' + mensagem.uuid + '">' +
-                    '<a class="pull-left" href="#">' +
-                        '<img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAACqUlEQVR4Xu2Y60tiURTFl48STFJMwkQjUTDtixq+Av93P6iBJFTgg1JL8QWBGT4QfDX7gDIyNE3nEBO6D0Rh9+5z9rprr19dTa/XW2KHl4YFYAfwCHAG7HAGgkOQKcAUYAowBZgCO6wAY5AxyBhkDDIGdxgC/M8QY5AxyBhkDDIGGYM7rIAyBgeDAYrFIkajEYxGIwKBAA4PDzckpd+322243W54PJ5P5f6Omh9tqiTAfD5HNpuFVqvFyckJms0m9vf3EY/H1/u9vb0hn89jsVj8kwDfUfNviisJ8PLygru7O4TDYVgsFtDh9Xo9NBrNes9cLgeTybThgKenJ1SrVXGf1WoVDup2u4jFYhiPx1I1P7XVBxcoCVCr1UBfTqcTrVYLe3t7OD8/x/HxsdiOPqNGo9Eo0un02gHkBhJmuVzC7/fj5uYGXq8XZ2dnop5Mzf8iwMPDAxqNBmw2GxwOBx4fHzGdTpFMJkVzNB7UGAmSSqU2RoDmnETQ6XQiOyKRiHCOSk0ZEZQcUKlU8Pz8LA5vNptRr9eFCJQBFHq//szG5eWlGA1ywOnpqQhBapoWPfl+vw+fzweXyyU+U635VRGUBOh0OigUCggGg8IFK/teXV3h/v4ew+Hwj/OQU4gUq/w4ODgQrkkkEmKEVGp+tXm6XkkAOngmk4HBYBAjQA6gEKRmyOL05GnR99vbW9jtdjEGdP319bUIR8oA+pnG5OLiQoghU5OElFlKAtCGr6+vKJfLmEwm64aosd/XbDbbyIBSqSSeNKU+HXzlnFAohKOjI6maMs0rO0B20590n7IDflIzMmdhAfiNEL8R4jdC/EZIJj235R6mAFOAKcAUYApsS6LL9MEUYAowBZgCTAGZ9NyWe5gCTAGmAFOAKbAtiS7TB1Ng1ynwDkxRe58vH3FfAAAAAElFTkSuQmCC">' +
-                        '</a>' +
-                        '<div class="media-body">' +
-                            '<small class="pull-right time"><i class="fa fa-clock-o"></i> ' + time + '</small>' +
-                            '<h5 class="media-heading">' + usuario.nome + '</h5>' +
-                            '<small class="col-lg-10">' + mensagem.mensagem + '</small>' +
-                        '</div>' +
-                    '</div>';
+            		  '<a class="pull-left" href="#">' +
+            		  '<img class="media-object" alt="Avatar" style="width: 32px; height: 32px;" src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg">' +
+            		  '</a>' +
+            		  '<div class="media-body">' +
+            		  '<small class="pull-right time"><i class="fa fa-clock-o"></i> ' + time + '</small>' +
+            		  '<h5 class="media-heading">' + usuario.nome + '</h5>' +
+            		  '<small class="col-lg-10">' + mensagem.mensagem + '</small>' +
+            		  '</div>' +
+            		  '</div>';
             
             $('.msg-wrap').append(msg);
             $('.msg-wrap')[0].scrollTop = $('.msg-wrap')[0].scrollHeight;
--- plugins/firebase-chat/0.8.0/chat.js
+++ plugins/firebase-chat/0.8.0/chat.js
@@ -1,5 +1,5 @@
 // TODO: aqui será trocado o usuário
-var usuario = {id : "palmeirense", nome: "Palmeirense"};
+var usuario = {id : "<id do usuário>", nome: "<nome do usuário>"};
 
 var lista = new Firebase('https://g6-chat.firebaseIO.com/chat/usuarios/');
 var chat = new Firebase('https://g6-chat.firebaseIO.com/chat/mensagens/' + usuario.id + '/');
@@ -36,31 +36,44 @@ $(document).ready(function() {
         
         var msg = null;
         if (mensagem.usuario.id == usuario.id) {
-            var msg = '<div class="row msg_container base_sent" data-uuid="' + mensagem.uuid + '">' +
-                                    '<div class="col-md-10 col-xs-10 ">' +
-                                        '<div class="messages msg_sent">' +
-                                            '<p>' + mensagem.mensagem + '</p>' +
-                                            '<time datetime="2009-11-13T20:00">' + DateUtils.time({timestamp: mensagem.timestamp}) + '</time>' +
-                                        '</div>' +
-                                    '</div>' +
-                                    '<div class="col-md-2 col-xs-2 avatar">' +
-                                        '<img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive ">' +
-                                    '</div>' +
-                                '</div>';
+            msg = '<div class="row msg_container base_sent" data-uuid="' + mensagem.uuid + '">' +
+            	  '<div class="col-md-10 col-xs-10 ">' +
+            	  '<div class="messages msg_sent">' +
+            	  '<p>' + mensagem.mensagem + '</p>' +
+            	  '<time datetime="2009-11-13T20:00">' + date.time({timestamp: mensagem.timestamp}) + '</time>' +
+            	  '</div>' +
+            	  '</div>' +
+            	  '<div class="col-md-2 col-xs-2 avatar">' +
+            	  '<img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive ">' +
+            	  '</div>' +
+            	  '</div>';
         } else {
             msg = '<div class="row msg_container base_receive" data-uuid="' + mensagem.uuid + '">' +
-                            '<div class="col-md-2 col-xs-2 avatar">' +
-                                '<img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive ">' +
-                            '</div>' +
-                            '<div class="col-xs-10 col-md-10">' +
-                                '<div class="messages msg_receive">' +
-                                    '<p>' + mensagem.mensagem + '</p>' +
-                                    '<time datetime="2009-11-13T20:00">' + DateUtils.time({timestamp: mensagem.timestamp}) + '</time>' +
-                                '</div>' +
-                            '</div>' +
-                        '</div>';
+            	  '<div class="col-md-2 col-xs-2 avatar">' +
+            	  '<img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive ">' +
+            	  '</div>' +
+            	  '<div class="col-xs-10 col-md-10">' +
+            	  '<div class="messages msg_receive">' +
+            	  '<p>' + mensagem.mensagem + '</p>' +
+            	  '<time datetime="2009-11-13T20:00">' + date.time({timestamp: mensagem.timestamp}) + '</time>' +
+            	  '</div>' +
+            	  '</div>' +
+            	  '</div>';
         }
 
         mostrarMensagem(msg);
+    });
+    
+    $(document).on('click', '.panel-heading span.icon_minim', function (e) {
+        var $this = $(this);
+        if (!$this.hasClass('panel-collapsed')) {
+            $this.parents('.panel').find('.panel-body').slideUp();
+            $this.addClass('panel-collapsed');
+            $this.removeClass('glyphicon-minus').addClass('glyphicon-plus');
+        } else {
+            $this.parents('.panel').find('.panel-body').slideDown();
+            $this.removeClass('panel-collapsed');
+            $this.removeClass('glyphicon-plus').addClass('glyphicon-minus');
+        }
     });
 });
